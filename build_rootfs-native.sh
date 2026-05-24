4c71b0b8e592381e3d632ca7f7086704913de901
Gold bug <Zxc20060226@outlook.com>
2026-05-24 23:50:47 +0800

    Update build_rootfs-native.sh

--- build_rootfs-native.sh
+++ build_rootfs-native.sh
@@ -5,10 +5,11 @@ DATE=$(date +%Y%m%d)      # 获取当前日期，格式如：20260523
 ARCH=$(uname -m)          # 获取当前系统架构，如：x86_64 或 aarch64
 ENABLE_binfmt="false"
 # 解析输入参数 (-i 指定 Dockerfile，-v 指定版本号)
-while getopts "i:v:a:b:c:d:e:f:" opt; do
+while getopts "i:v:K:a:b:c:d:e:f:" opt; do
   case $opt in
     i) DOCKERFILE="$OPTARG" ;; # -i 参数赋值给 DOCKERFILE 变量
     v) VERSION="$OPTARG" ;;    # -v 参数赋值给 VERSION 变量
+    K) BUILD_KDE="$OPTARG"  ;;
     a) ENABLE_binfmt="$OPTARG" ;; # -a 跨架构支持
     b) ENABLE_yj="$OPTARG" ;; 
     c) ENABLE_mesa="$OPTARG" ;;
@@ -75,6 +76,7 @@ echo "正在运行 Docker Build (原生模式)..."
 docker buildx build \
   --target export \
   --output type=tar,dest="$TEMP_TAR" \
+  --build-arg BUILD_KDE="$BUILD_KDE" \
   --build-arg ENABLE_binfmt_ARG="$ENABLE_binfmt" \
   --build-arg ENABLE_yj_ARG="$ENABLE_yj" \
   --build-arg ENABLE_mesa_ARG="$ENABLE_mesa" \
